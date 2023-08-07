58b048d85b20f2bf230786a01528a55480521ae7
berkay-yuksel <98.berkay.yuksel@gmail.com>
2023-08-07 21:55:23 +0300

    checkout session and page

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,20 +1,43 @@
-import Link from "next/link"
+import Link from "next/link";
 import Image from "next/image";
-import cartitemstyles from "src/styles/CartItem.module.css"
-export default  function CartItem({product}) {
+
+import cartitemstyles from "src/styles/CartItem.module.css";
+import { useShoppingCart ,formatCurrencyString } from "use-shopping-cart";
+export default function CartItem({ product }) {
+  const { setItemQuantity, removeItem } = useShoppingCart();
   return (
     <div className={cartitemstyles.cart_item_contaier}>
- <Image
-        
-          src={product.image}
-          width={75}
-          height={75}
-          alt="Picture of the author"
-        />
+      <div className={cartitemstyles.cart_name_and_pic_container}>
+        <Image src={product.image} width={75} height={75} alt={product.name} />
         <Link href={`/products/${product.id}`}>{product.name}</Link>
+      </div>
+
+      <div className={cartitemstyles.cart_rest_container}>
+        <div>
+          <button
+            disabled={product.quantity - 1 < 1}
+            onClick={() => setItemQuantity(product.id, product.quantity - 1)}
+          >
+            -
+          </button>
+          {`  ${product.quantity}  `}
+          <button
+            onClick={() => setItemQuantity(product.id, product.quantity + 1)}
+          >
+            +
+          </button>
+        </div>
+        <div>
+          {" "}
+          {formatCurrencyString({
+            currency: "usd",
+            value: product.price * product.quantity,
+          })}
+        </div>
+        <div>
+          <button onClick={() => removeItem(product.id)}>X</button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
-
-
-
--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,9 +1,30 @@
 import {  useShoppingCart } from "use-shopping-cart";
-import products from "../../products";
-import ProdCard from "src/components/ProdCard";
+import { useState } from "react";
 import CartItem from "src/components/CartItem"
+import axios  from "axios";
 export default function Cart() {
-const {cartCount,clearCart,cartDetails,formattedTotalPrice} = useShoppingCart()
+const {cartCount,clearCart,cartDetails,formattedTotalPrice,redirectToCheckout} = useShoppingCart()
+const [isRedirecting,setRedirecting]=useState(false)
+async function onCheckOut(){
+if(cartCount>0){
+  try{
+setRedirecting(true)
+const {id}= await axios.post('/api/checkout-sessions',cartDetails)
+.then(res=>res.data);
+const result= await redirectToCheckout(id);
+if(result?.error){
+  console.log("Error in result",result);
+}
+  } catch(error){
+console.log("Error",error);
+  }
+finally{
+  setRedirecting(false)
+}
+
+}
+
+}
 
   return (
     <div>
@@ -32,7 +53,8 @@ const {cartCount,clearCart,cartDetails,formattedTotalPrice} = useShoppingCart()
    cartCount>0 &&(
     <div> 
 <b>Total: {"  "} {formattedTotalPrice} </b>
-<button>Go to Checkout!</button>
+<button disabled={isRedirecting} onClick={onCheckOut}>   {isRedirecting ? "Redirecting.." :"Go to Checkout!"}</button>
+
     </div>
    )
 }
