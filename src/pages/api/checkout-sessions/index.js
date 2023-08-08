fab0fbe80fa5497c8e9fe6b34cccd4cfa645d6b0
berkay-yuksel <98.berkay.yuksel@gmail.com>
2023-08-09 01:18:50 +0300

    added shipping to session

--- src/pages/api/checkout-sessions/index.js
+++ src/pages/api/checkout-sessions/index.js
@@ -19,17 +19,26 @@ export default async function handler(req, res) {
         };
       });
       const lineItems = validateCartItems(products, cartDetails);
-      const session= await stripe.checkout.sessions.create({
+      const session = await stripe.checkout.sessions.create({
         mode: "payment",
-        payment_method_types: ['card'],
-        line_items:lineItems,
+        payment_method_types: ["card"],
+        line_items: lineItems,
+        shipping_address_collection: {
+          allowed_countries: ["US"],
+        },
+        shipping_options: [
+          {
+            shipping_rate: "shr_1NcyC1Aov6qqgCfc2nsClOYL",
+          },
+        ],
+        billing_address_collection: "auto",
         success_url: `${req.headers.origin}/success?session_id={CHECKOUT_SESSION_ID}`,
-        cancel_url:`${req.headers.origin}/cart`
-      })
-      res.status(200).json(session)
+        cancel_url: `${req.headers.origin}/cart`,
+      });
+      res.status(200).json(session);
     } catch (error) {
-        console.log(error)
-        res.status(500).json({statusCode:500,message:error.message})
+      console.log(error);
+      res.status(500).json({ statusCode: 500, message: error.message });
     }
   } else {
     res.setHeader("Allow", "POST");
